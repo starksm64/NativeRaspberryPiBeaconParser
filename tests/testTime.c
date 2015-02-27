5394589f53072a71942a11695470d7ca97ab7a70
starksm64 <starksm64@gmail.com>
2015-02-27 08:13:50 -0800

    Test long size and timeval to long conversion

--- tests/testTime.c
+++ tests/testTime.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include "../../bluez-5.28/lib/bluetooth.h"
 
 int main(int argc, char **argv) {
     struct timeval  tv;
@@ -17,4 +18,8 @@ int main(int argc, char **argv) {
     size_t length = strftime(timestr, 128, "%F %T", tm);
     snprintf(timestr+length, 128-length, ".%ld", tv.tv_usec/1000);
     printf("Time string: %s\n", timestr);
+
+    long ms = htobl(tv.tv_sec)*1000 + htobl(tv.tv_usec)/1000;
+    int64_t ms64 = htobl(tv.tv_sec)*1000 + htobl(tv.tv_usec)/1000;
+    printf("Sizeof(long) = %d, ms=%ld, ms64=%ld\n", sizeof(ms), ms, ms64);
 }
