2727abf48690fed0c1aa145bde9efa808185d8a7
starksm64 <starksm64@gmail.com>
2015-04-12 01:18:13 -0700

    sscanf tests

--- tests/testSscanf.cpp
+++ tests/testSscanf.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
     int secs, days, hrs, mins;
     const char *uptime = "uptime: 216095, days:2, hrs:12, min:1";
-    sscanf (uptime, "uptime: %d, days:%d, hrs:%d, min:%d", &secs, &days, &hrs, &mins);
-    printf("UP D:%d H:%d M:%d", days, hrs, mins);
+    int matched = sscanf (uptime, "uptime: %d, days:%d, hrs:%d, min:%d", &secs, &days, &hrs, &mins);
+    printf("matched:%d, UP D:%d H:%d M:%d", matched, days, hrs, mins);
 
 }
