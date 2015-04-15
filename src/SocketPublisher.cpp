d963eb732708849c52661809358512f91187c1a4
Scott Stark <starksm64@gmail.com>
2015-04-15 14:27:26 -0700

    debug logs from client hang

---
+++ src/SocketPublisher.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Scott Stark on 4/15/15.
+//
+
+#include "SocketPublisher.h"
---
+++ src/SocketPublisher.h
@@ -0,0 +1,14 @@
+//
+// Created by Scott Stark on 4/15/15.
+//
+
+#ifndef NATIVESCANNER_SOCKETPUBLISHER_H
+#define NATIVESCANNER_SOCKETPUBLISHER_H
+
+
+class SocketPublisher {
+
+};
+
+
+#endif //NATIVESCANNER_SOCKETPUBLISHER_H
