39f8cfef338b3d72ad3eabb6886b91ec0b3b363f
Scott Stark <starksm64@gmail.com>
2015-04-20 22:48:46 -0700

    Test sleep_for

---
+++ tests/threads/testThreadSleep.cpp
@@ -0,0 +1,36 @@
+
+#include <thread>
+#include <chrono>
+#include <ostream>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+static void hello() {
+    cout << "\tHello, I'm in a thread" << endl;
+    system_clock::time_point now = system_clock::now();
+    milliseconds ms = duration_cast< milliseconds >(now.time_since_epoch());
+    printf("Begin sleep_for(%lld), now=%lld\n", chrono::seconds(10).count(), ms.count());
+    chrono::seconds ten(10);
+    this_thread::sleep_for(ten);
+    now = system_clock::now();
+    ms = duration_cast< milliseconds >(now.time_since_epoch());
+    printf("End sleep_for(%lld), now=%lld\n", chrono::seconds(10).count(), ms.count());
+
+    now = system_clock::now();
+    ms = duration_cast< milliseconds >(now.time_since_epoch());
+    printf("Begin sleep_for(%lld), now=%lld\n", chrono::seconds(10).count(), ms.count());
+    this_thread::sleep_for(chrono::seconds(10));
+    now = system_clock::now();
+    ms = duration_cast< milliseconds >(now.time_since_epoch());
+    printf("End sleep_for(%lld), now=%lld\n", chrono::seconds(10).count(), ms.count());
+}
+
+
+int main() {
+    cout << "+++ Start a thread...\n";
+    thread t(hello);
+    t.join();
+    cout << "Joined the thread\n";
+}
