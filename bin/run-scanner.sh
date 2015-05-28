68cd9616f7f8c69722c24ebb207b6269e1a29d22
starksm <starksm64@gmail.com>
2015-05-28 13:49:09 -0700

    Look to scanner.conf for username/password arguments

--- bin/run-scanner.sh
+++ bin/run-scanner.sh
@@ -10,6 +10,8 @@ ROOT=`dirname $BIN`
 # - scannerID ; the name by which the scanner is defined in the beacon event messages
 # - heartbeatUUID ; the uuid of the beacon used as the heartbeat for the scanner
 # - brokerURL ; the url of the activemq broker
+# - username ; the username to authenticate against the broker
+# - password ; the password to authenticate against the broker
 . ~/scanner.conf
 
 # Aliases for backward compatibility to previous variables used from scanner.conf
@@ -25,6 +27,13 @@ fi
 
 CMD="${ROOT}/Debug/src2/NativeScannerBlueZ"
 ARGS="--scannerID "${scannerID:-`hostname`}" --brokerURL "${brokerURL:-192.168.1.107:5672}" --heartbeatUUID "${heartbeatUUID}""
+if [ -n "$username" ]; then
+        ARGS="${ARGS} --username ${username}"
+fi
+if [ -n "$password" ]; then
+        ARGS="${ARGS} --password ${password}"
+fi
+
 BACKGROUND=""
 # Check for a background argument
 if [ "$1" == "-background" ]; then
