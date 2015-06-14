b838248996a361e5064ad729880fe4c73e74ea95
Scott Stark <starksm64@gmail.com>
2015-06-13 19:11:53 -0700

    Have non-lcd version of ScannerView display when the closest beacon changes in the log

--- src2/MockScannerView.h
+++ src2/MockScannerView.h
@@ -8,9 +8,21 @@
 #include "ScannerView.h"
 
 class MockScannerView : public ScannerView {
+private:
+    int closestID;
+    string closestName;
+
 public:
 
     virtual void displayBeacon(const Beacon &beacon) {
+        int minorID = beacon.getMinor();
+        if(minorID != closestID) {
+            closestID = minorID;
+            if(getBeaconMapper()) {
+                closestName = getBeaconMapper()->lookupUser(minorID);
+            }
+            printf("+++ New closest beacon: (%d,%s)\n", closestID, closestName.c_str());
+        }
     }
     virtual void displayHeartbeat(const Beacon &beacon) {
     }
