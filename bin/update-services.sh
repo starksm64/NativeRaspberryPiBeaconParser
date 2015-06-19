845d04bbe6758bbfbca928baa36c22dd4a61e194
starksm <starksm64@gmail.com>
2015-06-19 10:44:00 -0700

    Use /etc/systemd/system/ as path

--- bin/update-services.sh
+++ bin/update-services.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# A script that copies any newer service file in ../systemd into the /lib/systemd/system directory
+# A script that copies any newer service file in ../systemd into the /etc/systemd/system directory
 # and refreshes the systemd with the new versions
 
 for service in $(ls ~/NativeRaspberryPiBeaconParser/systemd/*.service); do
     sfile=${service##*/}
-    if [ $service -nt /lib/systemd/system/$sfile ]; then
+    if [ $service -nt /etc/systemd/system/$sfile ]; then
         echo $service is newer that installed service
-        cp $service /lib/systemd/system/
+        cp $service /etc/systemd/system/
     fi
 done
 systemctl --system daemon-reload
