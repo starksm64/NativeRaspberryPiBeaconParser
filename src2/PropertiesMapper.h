8a83b74015d09a4896a05f373d486a48d27e3f4c
starksm64 <man$Ir0n>
2015-09-09 17:18:34 -0700

    Fix info.second typo

--- src2/PropertiesMapper.h
+++ src2/PropertiesMapper.h
@@ -34,7 +34,7 @@ public:
             string pair = mappings.substr(last, next-last);
             std::pair<string,string> info = splitUser(pair);
             mapping[info.first] = info.second;
-            printf("\t%d=%s\n", info.first, info.seconf.c_str());
+            printf("\t%d=%s\n", info.first, info.second.c_str());
             last = next + 1;
         }
         std::pair<string,string> info = splitUser(mappings.substr(last));
